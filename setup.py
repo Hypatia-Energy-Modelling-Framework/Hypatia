30915ab23cc92a5425b90cc0c2e0d62debcabde9
Negnamazi <62033507+Negnamazi@users.noreply.github.com>
2021-12-10 12:21:06 +0100

    adding manifest.in

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="hypatia",
+    name="hypatia_test",
     description=(
         "An Operation and Planning Energy System Modelling Framework in Python"
     ),
